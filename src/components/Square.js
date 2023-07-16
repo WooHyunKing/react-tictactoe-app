06b5f6c865840b1999db293a4daa41533b423a24
신 우현 <woo_hyun_1@naver.com>
2023-07-16 15:50:43 +0900

    [Chap 3] Migration class component to function component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import "./Square.css";
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
+const Square = ({ onClick, value }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
 
 export default Square;
+
+// class Square extends Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
